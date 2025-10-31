6b1cbeeff9670c77685f8a115ff29b7d49c01e0b
vadiraj-22 <vadirajjoshi22504@gmail.com>
2025-10-31 17:59:22 +0530

    changes in constants index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -130,7 +130,7 @@ export const myProjects = [
   ],
 },
   {
-    title: 'CarePulse - Health Management System',
+    title: 'CarePulse',
     desc: 'An innovative healthcare platform designed to streamline essential medical processes. It simplifies patient registration, appointment scheduling, and medical record management, providing a seamless experience for both healthcare providers and patients.',
     subdesc:
       'With a focus on efficiency, CarePulse integrantes complex forms and SMS notifications, by using Next.js, Appwrite, Twillio and Sentry that enhance operational workflows.',
