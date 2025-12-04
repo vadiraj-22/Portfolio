a2ff4bccd03e2b4ac3496f21623a02ffcb613407
vadiraj-22 <vadirajjoshi22504@gmail.com>
2025-12-04 21:44:04 +0530

    updated project  section

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -64,7 +64,9 @@ const About = () => {
             <div>
               <p className="grid-headtext">I’m very flexible with time zone communications & locations</p>
               <p className="grid-subtext">I&apos;m based in Karnataka, India and open to remote work worldwide.</p>
-              <Button name="Contact Me" isBeam containerClass="w-full mt-10" />
+              <a href="#contact" className='w-fit'>
+                <Button name="Contact Me" isBeam containerClass="w-full mt-10" />
+              </a>
             </div>
           </div>
         </div>
