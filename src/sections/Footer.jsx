ed03e248c5326861333c4de16bca5076c9664901
vadiraj-22 <vadirajjoshi22504@gmail.com>
2026-02-07 09:57:29 +0530

    footer responsiveness fixed

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 
 const Footer = () => {
     return (
-        <section className='c-space pt-7 pb-3 border-t border-blaak-300 flex justify-between items-center flex-wrap gap-5 '>
-            <div className='text-white-500 flex  gap-3'>
+        <section className='c-space pt-7 pb-3 border-t  border-white flex justify-between items-center flex-wrap gap-5 '>
+            <div className='text-white-500 flex mx-auto  gap-3'>
                 <p>Terms & Conditions </p>
                 <p>|</p>
                 <p>Privacy Policy</p>
             </div>
-            <div className='flex gap-3'>
+            <div className='flex mx-auto gap-3'>
                 <a href="https://github.com/vadiraj-22" target="_blank" rel="noopener noreferrer" className='social-icon'>
                     <img src="/assets/github.svg" alt="github" className='w-1/2 h-1/2' />
                 </a>
@@ -25,7 +25,7 @@ const Footer = () => {
                     <img src="/assets/twitter.svg" alt="github" className='w-1/2 h-1/2' />
                 </div> */}
             </div>
-            <p className='text-white-500'>  @{new Date().getFullYear()}  Vadiraj Joshi, All right Reserved.</p>
+            <p className='text-white-500 mx-auto'>  @{new Date().getFullYear()}  Vadiraj Joshi, All right Reserved.</p>
         </section>
     )
 }
